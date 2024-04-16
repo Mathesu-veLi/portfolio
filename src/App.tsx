90d441a3eafb83e23b3450a2e820e91e0e2fd8d5
Matheus Levi <matheuslevit@gmail.com>
2024-04-15 23:26:41 -0300

    remove margin bottom

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import { Welcome } from "./components/Welcome";
 
 function App() {
   return (
-    <div className="m-5 flex flex-col justify-center items-center h-full">
+    <div className="m-5 mb-0 flex flex-col justify-center items-center h-full">
       <Header />
 
-      <div className="flex flex-col gap-36">
+      <div className="flex gap-36">
         <Welcome />
 
         <ProjectsDashboard />
