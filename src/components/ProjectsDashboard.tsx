9fd4eb0cbebeb566f86cdf9885825ff95fbeb098
Matheus Levi <matheuslevit@gmail.com>
2024-04-15 23:26:30 -0300

    creates a carousel to projects

--- src/components/ProjectsDashboard.tsx
+++ src/components/ProjectsDashboard.tsx
@@ -1,31 +1,53 @@
 import { Project } from "./Project";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export function ProjectsDashboard() {
   return (
-    <div className="grid gap-10">
+    <div>
       <h1 className="text-center font-light text-3xl tracking-wider">
         Projects
       </h1>
 
-      <div className="grid lg:grid-cols-3 gap-10 lg:gap-20">
-        <Project
-          name="OUV Costumes"
-          shortDescription="An online store for team shirts"
-          imgUrl="https://i.ibb.co/LZdZBtg/homescreen.jpg"
-          link="https://ouv.vercel.app"
-        />
-        <Project
-          name="Accountz"
-          shortDescription="An account manager"
-          imgUrl="https://i.ibb.co/hHQGD3G/dashboard.jpg"
-          link="https://accountz.vercel.app"
-        />
-        <Project
-          name="DEV Toolkit"
-          shortDescription="A basic set of tools for devs"
-          imgUrl="https://i.ibb.co/YPpSZ7s/dev-home.png"
-          link="https://devfordev.vercel.app"
-        />
+      <div className="gap-10 lg:gap-20 flex justify-center items-center my-16">
+        <Carousel
+          className="w-10/12"
+          orientation="vertical"
+        >
+          <CarouselPrevious />
+          <CarouselContent>
+            <CarouselItem className="basis-1/3">
+              <Project
+                name="OUV Costumes"
+                shortDescription="An online store for team shirts"
+                imgUrl="https://i.ibb.co/LZdZBtg/homescreen.jpg"
+                link="https://ouv.vercel.app"
+              />
+            </CarouselItem>
+            <CarouselItem className="basis-1/3">
+              <Project
+                name="Accountz"
+                shortDescription="An account manager"
+                imgUrl="https://i.ibb.co/hHQGD3G/dashboard.jpg"
+                link="https://accountz.vercel.app"
+              />
+            </CarouselItem>
+            <CarouselItem className="basis-1/3">
+              <Project
+                name="DEV Toolkit"
+                shortDescription="A basic set of tools for devs"
+                imgUrl="https://i.ibb.co/YPpSZ7s/dev-home.png"
+                link="https://devfordev.vercel.app"
+              />
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselNext />
+        </Carousel>
       </div>
     </div>
   );
