a9579f89a869945ac55ed9593d3acdff764ee2db
Matheus Levi <matheuslevit@gmail.com>
2024-04-15 22:33:04 -0300

    edit style to mobile devices

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -8,16 +8,17 @@ interface ProjectProps {
 export function Project(props: ProjectProps) {
   return (
     <a href={props.link}>
-      <div className="flex justify-around items-center">
-        <h1 className="font-semibold text-lg">{props.name}</h1>
-        <p className="text-gray-300 max-w-44">{props.shortDescription}</p>
-      </div>
-      <img
-        src={props.imgUrl}
-        alt="Project screenshot"
-        className="w-full border rounded-sm"
-      />
-    </div>
+        <div className="p-3 rounded-lg border flex flex-col-reverse justify-between lg:h-80 h-64">
+          <div className="flex justify-around items-center lg:h-14">
+            <h1 className="font-semibold text-lg">{props.name}</h1>
+            <p className="text-gray-400 max-w-52 text-center">{props.shortDescription}</p>
+          </div>
+          <img
+            src={props.imgUrl}
+            alt="Project screenshot"
+            className="w-full border rounded-sm"
+          />
+        </div>
     </a>
   );
 }
