cdaa0bc95c8f4fa2642adaa6fea0c3b7d0f38c83
Matheus Levi <matheuslevit@gmail.com>
2024-04-15 22:20:57 -0300

    show the name of the projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -2,18 +2,17 @@ interface ProjectProps {
   name: string;
   shortDescription: string;
   imgUrl: string;
-  languages: string[];
 }
 
-export function Project() {
+export function Project(props: ProjectProps) {
   return (
     <div className="p-3 rounded-lg border flex flex-col-reverse gap-2">
       <div className="flex justify-around items-center">
-        <h1 className="font-semibold text-lg">Project name</h1>
-        <p className="text-gray-300 max-w-44">Short Description</p>
+        <h1 className="font-semibold text-lg">{props.name}</h1>
+        <p className="text-gray-300 max-w-44">{props.shortDescription}</p>
       </div>
       <img
-        src=""
+        src={props.imgUrl}
         alt="Project screenshot"
         className="w-full border rounded-sm"
       />
