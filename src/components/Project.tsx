19d1b6697f2fef245ab39b0a856a3f557acbf853
Matheus Levi <matheuslevit@gmail.com>
2024-04-15 23:31:29 -0300

    remove the short description

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -8,17 +8,16 @@ interface ProjectProps {
 export function Project(props: ProjectProps) {
   return (
     <a href={props.link} target="_blank">
-        <div className="p-3 rounded-lg border flex flex-col-reverse justify-between lg:items-center">
-          <div className="flex justify-around items-center lg:h-14 w-full lg:px-12 mt-2">
-            <h1 className="font-semibold text-lg">{props.name}</h1>
-            <p className="text-gray-400 max-w-52 text-center">{props.shortDescription}</p>
-          </div>
-          <img
-            src={props.imgUrl}
-            alt="Project screenshot"
-            className="w-full lg:w-[460px] border rounded-sm"
-          />
+      <div className="p-3 rounded-lg border flex flex-col-reverse justify-between lg:items-center">
+        <div className="flex justify-around items-center lg:h-14 w-full mt-2">
+          <h1 className="font-semibold text-lg">{props.name}</h1>
         </div>
+        <img
+          src={props.imgUrl}
+          alt="Project screenshot"
+          className="w-full lg:w-[460px] border rounded-sm"
+        />
+      </div>
     </a>
   );
 }
