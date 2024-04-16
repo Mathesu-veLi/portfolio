b9286d762bc63cca593b6c18a99612d481f3810a
Matheus Levi <matheuslevit@gmail.com>
2024-04-15 22:43:43 -0300

    change style of project components

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -7,16 +7,16 @@ interface ProjectProps {
 
 export function Project(props: ProjectProps) {
   return (
-    <a href={props.link}>
-        <div className="p-3 rounded-lg border flex flex-col-reverse justify-between lg:h-80 h-64">
-          <div className="flex justify-around items-center lg:h-14">
+    <a href={props.link} target="_blank">
+        <div className="p-3 rounded-lg border flex flex-col-reverse justify-between lg:items-center">
+          <div className="flex justify-around items-center lg:h-14 w-full lg:px-12 mt-2">
             <h1 className="font-semibold text-lg">{props.name}</h1>
             <p className="text-gray-400 max-w-52 text-center">{props.shortDescription}</p>
           </div>
           <img
             src={props.imgUrl}
             alt="Project screenshot"
-            className="w-full border rounded-sm"
+            className="w-full lg:w-[460px] border rounded-sm"
           />
         </div>
     </a>
